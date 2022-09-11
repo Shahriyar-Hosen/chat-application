b1424ec6ebb1208f59abe83f576e5197c8057c4c
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-11 10:00:01 +0600

    base url add in env

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:9000",
+    baseUrl: process.env.REACT_APP_API_URL,
   }),
   endpoints: (builder) => ({}),
 });
