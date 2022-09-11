c7b921eccec269f523af2032b7ff4bc7cb16d90e
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-11 09:56:04 +0600

    Store configuration

--- SETUP.md
+++ SETUP.md
@@ -19,3 +19,18 @@
         }),
     });
 ```
+
+#### 2. Store configuration
+
+```sh
+    import { configureStore } from "@reduxjs/toolkit";
+    import { apiSlice } from "../features/api/apiSlice";
+
+    export const store = configureStore({
+    reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer,
+    },
+    middleware: (getDefaultMiddlewares) =>
+        getDefaultMiddlewares().concat(apiSlice.middleware),
+    });
+```
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "../features/api/apiSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddlewares) =>
+    getDefaultMiddlewares().concat(apiSlice.middleware),
 });
