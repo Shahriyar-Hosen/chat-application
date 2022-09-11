5c4d2535767177abcd311a21ee0141d03e8e56c9
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-11 08:23:45 +0600

    project setup done

--- src/App.js
+++ src/App.js
@@ -1,57 +1,21 @@
-import React from 'react';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Conversation from "./pages/Conversation";
+import Inbox from "./pages/Inbox";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={'logo'} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <section>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/inbox" element={<Conversation />} />
+        <Route path="/inbox/:id" element={<Inbox />} />
+      </Routes>
+    </section>
   );
-}
+};
 
 export default App;
