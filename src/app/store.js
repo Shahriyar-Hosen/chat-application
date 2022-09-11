12cc40372d2b6809c21d3ade580b16c2ff3fd8df
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-11 12:13:14 +0600

    added reducer in store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "../features/api/apiSlice";
+import authSliceReducer from "../features/auth/authSlice";
+import conversationSliceReducer from "../features/conversation/conversationSlice";
+import messagesSliceReducer from "../features/messages/messagesSlice";
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authSliceReducer,
+    conversation: conversationSliceReducer,
+    message: messagesSliceReducer,
   },
+  devTools: process.env.NODE_ENV !== "production",
   middleware: (getDefaultMiddlewares) =>
     getDefaultMiddlewares().concat(apiSlice.middleware),
 });
