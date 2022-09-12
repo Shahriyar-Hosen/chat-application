2f05fd7ed96cce545b2e7d53f31aac85a0fbc2cd
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-12 09:52:17 +0600

    Login done

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -57,8 +57,6 @@ const Register = () => {
           <form
             onSubmit={handleSubmit}
             className="mt-8 space-y-6"
-            action="#"
-            method="POST"
           >
             <input type="hidden" name="remember" value="true" />
             <div className="rounded-md shadow-sm -space-y-px">
