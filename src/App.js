9150bf177126f07b255758d68742f73b331b50a4
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-12 11:22:47 +0600

    Checking Authentication

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import useAuthCheck from "./hooks/useAuthCheck";
 import Conversation from "./pages/Conversation";
 import Inbox from "./pages/Inbox";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 const App = () => {
-  return (
-    <section>
+  const authLoading = useAuthCheck();
+
+  return authLoading ? (
+    <div>Checking Authentication ...</div>
+  ) : (
+    <main>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/inbox" element={<Conversation />} />
         <Route path="/inbox/:id" element={<Inbox />} />
       </Routes>
-    </section>
+    </main>
   );
 };
 
