436e21e79d42380d96082a93a8b3789d14d4fc9a
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-12 11:58:59 +0600

    logout fetcher added done

--- src/components/inbox/Navigation.js
+++ src/components/inbox/Navigation.js
@@ -1,25 +1,34 @@
 import { Link } from "react-router-dom";
 import logoImage from "../../assets/images/lws-logo-dark.svg";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { userLoggedOut } from "../../features/auth/authSlice";
 
-export default function Navigation() {
-    return (
-        <nav className="border-general sticky top-0 z-40 border-b bg-violet-700 transition-colors">
-            <div className="max-w-7xl mx-auto">
-                <div className="flex justify-between h-16 items-center">
-                    <Link to="/">
-                        <img
-                            className="h-10"
-                            src={logoImage}
-                            alt="Learn with Sumit"
-                        />
-                    </Link>
-                    <ul>
-                        <li className="text-white">
-                            <a href="#">Logout</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
-}
+const Navigation = () => {
+  const dispatch = useDispatch();
+
+  const logout = () => {
+    dispatch(userLoggedOut());
+    localStorage.clear();
+  };
+  return (
+    <nav className="border-general sticky top-0 z-40 border-b bg-violet-700 transition-colors">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex justify-between h-16 items-center">
+          <Link to="/">
+            <img className="h-10" src={logoImage} alt="Learn with Sumit" />
+          </Link>
+          <ul>
+            <li className="text-white">
+              <span className="cursor-pointer" onClick={logout}>
+                Logout
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
