5f783b78d2871de6d653fcb967bcda949d98d6ce
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-13 15:58:45 +0600

    ChatHead done

--- src/components/inbox/chatbody/ChatHead.js
+++ src/components/inbox/chatbody/ChatHead.js
@@ -1,12 +1,25 @@
-export default function ChatHead({ avatar, name }) {
-    return (
-        <div className="relative flex items-center p-3 border-b border-gray-300">
-            <img
-                className="object-cover w-10 h-10 rounded-full"
-                src={avatar}
-                alt={name}
-            />
-            <span className="block ml-2 font-bold text-gray-600">{name}</span>
-        </div>
-    );
-}
+import React from "react";
+import { useSelector } from "react-redux";
+import gravatarUrl from "gravatar-url";
+
+const ChatHead = ({ message }) => {
+  const email = useSelector((state) => state.auth.user);
+  const { sender, receiver } = message || {};
+
+  const partnerEmail = sender.email === email ? receiver.email : sender.email;
+
+  const partnerName = sender.email === email ? receiver.name : sender.name;
+
+  return (
+    <div className="relative flex items-center p-3 border-b border-gray-300">
+      <img
+        className="object-cover w-10 h-10 rounded-full"
+        src={gravatarUrl(partnerEmail)}
+        alt={partnerName}
+      />
+      <span className="block ml-2 font-bold text-gray-600">{partnerName}</span>
+    </div>
+  );
+};
+
+export default ChatHead;
