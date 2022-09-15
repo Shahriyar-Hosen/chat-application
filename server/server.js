a3d3c12b6cb85b01a9288588a52c463a9c4af559
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-15 15:37:43 +0600

    server response middleware added done

--- server/server.js
+++ server/server.js
@@ -10,6 +10,31 @@ const io = require("socket.io")(server);
 global.io = io;
 
 const router = jsonServer.router("db.json");
+
+// response middleware
+router.render = (req, res) => {
+  const path = req.path;
+  const method = req.method;
+
+  if (
+    path.includes("/conversations") &&
+    (method === "POST" || method === "PATCH")
+  ) {
+    // emit socket
+    io.emit("conversation", {
+      data: res.locals.data,
+    });
+  }
+
+  if (path.includes("/messages") && method === "POST") {
+    // emit socket
+    io.emit("message", {
+      data: res.locals.data,
+    });
+  }
+
+  res.json(res.locals.data);
+};
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 9000;
 
