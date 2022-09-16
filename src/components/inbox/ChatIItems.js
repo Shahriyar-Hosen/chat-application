bb2b924457d366c89090a5bc0dc7e21f5dfe29b6
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-16 15:42:15 +0600

    InfiniteScroll install

--- src/components/inbox/ChatIItems.js
+++ src/components/inbox/ChatIItems.js
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 import gravatarUrl from "gravatar-url";
 import moment from "moment/moment";
 import getPartnerInfo from "../../utils/getPartnerInfo";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const ChatIItems = () => {
   const { user } = useSelector((state) => state.auth) || {};
@@ -23,42 +24,48 @@ const ChatIItems = () => {
 
   if (isLoading) {
     content = <li className="m-2 text-center">Loading...</li>;
-
   } else if (!isLoading && isError) {
     content = (
       <li className="m-2 text-center">
         <Error message={error?.data} />
       </li>
-
     );
   } else if (!isLoading && !isError && conversations?.length === 0) {
     content = <li className="m-2 text-center">No conversations found!</li>;
-
   } else if (!isLoading && !isError && conversations?.length > 0) {
-    
-    content = conversations.map((conversation) => {
-      const { id, message, timestamp } = conversation;
-      const { email } = user || {};
-      const { name, email: partnerEmail } = getPartnerInfo(
-        conversation?.users,
-        email
-      );
-      
-      return (
-        <li key={id}>
-          <Link to={`/inbox/${id}`}>
-            <ChatItem
-              avatar={gravatarUrl(partnerEmail, {
-                size: 80,
-              })}
-              name={name}
-              lastMessage={message}
-              lastTime={moment(timestamp).fromNow()}
-            />
-          </Link>
-        </li>
-      );
-    });
+    content = (
+      <InfiniteScroll
+        dataLength={conversations.length}
+        next={() => console.log("fetchData")}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+        height={window.innerHeight - 129}
+      >
+        {conversations.map((conversation) => {
+          const { id, message, timestamp } = conversation;
+          const { email } = user || {};
+          const { name, email: partnerEmail } = getPartnerInfo(
+            conversation?.users,
+            email
+          );
+
+          return (
+            <li key={id}>
+              <Link to={`/inbox/${id}`}>
+                <ChatItem
+                  avatar={gravatarUrl(partnerEmail, {
+                    size: 80,
+                  })}
+                  name={name}
+                  lastMessage={message}
+                  lastTime={moment(timestamp).fromNow()}
+                />
+              </Link>
+            </li>
+          );
+        })}
+      </InfiniteScroll>
+    );
   }
 
   return <ul>{content}</ul>;
